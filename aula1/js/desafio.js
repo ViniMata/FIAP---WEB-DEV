814edef34789d512a460566d66ce7921a39d31ab
ViniMata <vinicius.matareli2005@gmail.com>
2024-04-03 22:46:48 -0300

    att

---
+++ aula1/js/desafio.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome:")
+var idade = prompt("Digite sua idade:")
+var profissao = prompt("Onde você trabalha? :")
+
+confirm(`Esta certa as informações?
+Olá ${nome} você tem ${idade} e trabalha como ${profissao }`)
---
+++ aula1/js/script.js
@@ -0,0 +1,16 @@
+/* alert("Olá Mundo!");
+prompt("Digite seu nome:");
+confirm("Voce deseja sair?");
+ */
+//para comentar -> alt + ahift + 'a'.
+//Entrada
+var nome = prompt("Digite seu nome:");
+var idade = prompt("Digite sua idade:");
+var profissao= prompt
+//Saida
+console.log(`Olá ${nome} fiquei sabendo que voce tem ${idade} anos`)
+
+
+
+
+
