e0da553ae18775e66bb1f912ed1a85dbb9d878e4
ViniMata <vinicius.matareli2005@gmail.com>
2024-04-10 22:44:03 -0300

    att

--- aula2/app.js
+++ aula2/app.js
@@ -1,4 +1,4 @@
-function clicar(){
+function soma(){
     //Entrada
     var n1 = parseFloat(document.getElementById("numero1").value)
     var n2 = parseFloat(document.getElementById("numero2").value)
@@ -7,4 +7,34 @@ function clicar(){
     var soma = n1 + n2
     //Saida
     resultado.innerText = soma
+}
+function subtracao(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById("numero1").value)
+    var n2 = parseFloat(document.getElementById("numero2").value)
+    var resultado = document.getElementById("resultado")
+    //Processamento
+    var subtracao = n1 - n2
+    //Saida
+    resultado.innerText = subtracao
+}
+function divisao(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById("numero1").value)
+    var n2 = parseFloat(document.getElementById("numero2").value)
+    var resultado = document.getElementById("resultado")
+    //Processamento
+    var divisao = n1 / n2
+    //Saida
+    resultado.innerText = divisao
+}
+function multiplicacao(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById("numero1").value)
+    var n2 = parseFloat(document.getElementById("numero2").value)
+    var resultado = document.getElementById("resultado")
+    //Processamento
+    var multiplicacao = n1 * n2
+    //Saida
+    resultado.innerText =multiplicacao
 }
