6328d931fab65be68285f6b79ae43639cebdac01
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-05-15 22:49:37 -0300

    att

---
+++ aula6/Desafio1.js
@@ -0,0 +1,7 @@
+const numeros = [1,2,3]
+
+function dobrar(array){
+    return array.map(numero => numero*2)
+}
+
+console.log(dobrar([85,96,25]))
---
+++ aula6/desafio2.js
@@ -0,0 +1,5 @@
+function upperCase(strings){
+    return strings.map(word =>word.toUpperCase())
+}
+
+console.log(upperCase(["sao","paulo", "futebol", "clube"]))
