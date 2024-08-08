c91ddf204837ffb4b2c2d17f44769746c588f5c3
ViniMata <vinicius.matareli2005@gmail.com>
2024-08-07 22:48:17 -0300

    att

--- aula7/app.js
+++ aula7/app.js
@@ -2,22 +2,22 @@ const btnCriar = document.querySelector("#btnCriar")
 const inputUsuario = document.querySelector("#inputUsuario")
 const listaFilmes = document.querySelector("#listaFilmes")
 
-btnCriar.addEventListener('click', function(infosDoEvento){
+btnCriar.addEventListener('click', function (infosDoEvento) {
     infosDoEvento.preventDefault();
-    
+
     const novoFilme = document.createElement("li")
     novoFilme.innerText = inputUsuario.value;
 
     const btnEditar = document.createElement('button')
     btnEditar.innerText = "Editar"
-    btnEditar.addEventListener('click', function(){
+    btnEditar.addEventListener('click', function () {
         novoFilme.style.color = "red";
         novoFilme.classList.toggle("fundo-preto")
     })
 
     const imagem = document.createElement('img')
-    imagem.src=""
-    novoFilme.innerHTML=`
+    imagem.src = ""
+    novoFilme.innerHTML = `
         <h1> ${inputUsuario.value} </h1>
         <p>Avaliação do filme</p>
     `
