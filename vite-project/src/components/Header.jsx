6d6d0dd959c82d8c24043f98a363fd967d58b683
ViniMata <vinicius.matareli2005@gmail.com>
2024-08-14 22:46:43 -0300

    att

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    
+    const nome = "Tiquinho Soares";
+    return ( 
+        <>
+            <h1>olá, {nome}</h1>
+            <input type="text" name="" id="" className="" />
+        </>
+     );
+}
+
+export default Header;
