309d0f485ae008ede0cfdcf541b1625f746d4df4
ViniMata <vinicius.matareli2005@gmail.com>
2024-09-04 22:50:32 -0300

    att

--- react_css/react_css/src/App.jsx
+++ react_css/react_css/src/App.jsx
@@ -12,6 +12,7 @@ const estiloTitulo = {
       <h1 className="titulo"
       style={estiloTitulo}
       >Titulo da pagina</h1>
+      <p className="text-fiapPrimary text-5xl w-[350px] md:text-green-600 bg-gray-400 ml-3 flex justify-center">Paragrafo</p>
     </>
   )
 }
